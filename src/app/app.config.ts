1e97b23de068dfe9513da6945ababa254695ad94
Luigi Federico <luigi.federico94@outlook.it>
2025-06-17 18:04:34 +0200

    commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideCharts } from 'ng2-charts'; // ✅ Nome corretto del provider
+
 
 import { routes } from './app.routes';
 
@@ -8,6 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideCharts()
+
   ]
 };
