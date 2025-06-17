cab5a0232df626d16594969a12c380b4cf77057c
Luigi Federico <luigi.federico94@outlook.it>
2025-06-17 17:57:35 +0200

    altre funzionalità inserite

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideCharts } from 'ng2-charts'; // ✅ Nome corretto del provider
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideCharts()
+  ]
 };
--- src/app/components/show-progress/show-progress.component.ts
+++ src/app/components/show-progress/show-progress.component.ts
@@ -1,24 +1,43 @@
 import { Component } from '@angular/core';
 import { ExerciseProgressWithName } from '../../../Interface/IWorkout';
 import { SupabaseService } from '../../services/supabase.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-show-progress',
-  imports: [],
   templateUrl: './show-progress.component.html',
-  styleUrl: './show-progress.component.css'
+   standalone: true,
+  imports: [
+    CommonModule,FormsModule
+  ],
+  styleUrls: ['./show-progress.component.css']
 })
 export class ShowProgressComponent {
   progressi: ExerciseProgressWithName[] = [];
+  eserciziUnici: string[] = [];
+esercizioSelezionato = '';
+  progressiFiltrati: ExerciseProgressWithName[] = [];
 
   constructor(private supabase: SupabaseService) {}
 
-  async ngOnInit(): Promise<void> {
+   async ngOnInit(): Promise<void> {
     try {
       this.progressi = await this.supabase.getExerciseProgressWithName();
+
+      // Otteniamo i nomi unici degli esercizi
+      this.eserciziUnici = Array.from(
+        new Set(this.progressi.map(p => p.exercise_name))
+      );
+
+      this.esercizioSelezionato = this.eserciziUnici[0] ?? ''; // seleziona il primo di default
+
     } catch (error) {
       console.error('Errore caricamento progressi:', error);
     }
   }
 
+  getProgressiFiltrati(): ExerciseProgressWithName[] {
+    return this.progressi.filter(p => p.exercise_name === this.esercizioSelezionato);
+  }
 }
